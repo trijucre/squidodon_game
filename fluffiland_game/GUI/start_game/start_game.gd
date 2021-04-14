03dc0d96c105c73f5ea48be9cd985b8e714ad8de
trijucre <trijucre@gmail.com>
2021-04-14 19:51:11 +0200

    added start game scene

---
+++ fluffiland_game/GUI/start_game/start_game.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_continue_pressed():
+	pass # Replace with function body.
+
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
